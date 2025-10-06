72fcfa640005361bade0a146307476394c42034a
Pundima-Lakshan <pundimal@gmail.com>
2025-10-07 04:40:31 +0530

    bash, hypr, nvim, waybar

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,19 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = function(_, opts)
+    -- add tsx and treesitter
+    vim.list_extend(opts.ensure_installed, {
+      "bash",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "regex",
+      "tsx",
+      "typescript",
+      "vim",
+      "yaml",
+    })
+  end,
+}
