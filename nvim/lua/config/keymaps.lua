214cb24e3467e1dde58c3d865fc0d363dbf37564
pundimal <pundimal@gmail.com>
2025-10-20 05:37:53 +0530

    clangd

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -8,16 +8,21 @@ vim.keymap.set("n", "<leader>rb", function()
     vim.notify("No project root or build.bat found", vim.log.levels.WARN)
     return
   end
-
   local root_dir = vim.fs.dirname(root)
   local build_file = root_dir .. "/build.bat"
-
   if vim.fn.filereadable(build_file) == 1 then
-    vim.cmd("!cd " .. root_dir .. " && build.bat")
+    local args = "clean build"
+    local cmd = "cd " .. root_dir .. " && build.bat " .. vim.fn.shellescape(args)
+    local output = vim.fn.system(cmd)
+    if vim.v.shell_error ~= 0 then
+      vim.notify("Error running build.bat:\n" .. output, vim.log.levels.ERROR)
+    else
+      vim.notify("build.bat output:\n" .. output, vim.log.levels.INFO)
+    end
   else
     vim.notify("build.bat not found in " .. root_dir, vim.log.levels.ERROR)
   end
-end, { desc = "Run build.bat in project root" })
+end, { desc = "Run build.bat in project root with arguments and show output" })
 
 vim.keymap.set("n", "<leader>rd", function()
   local root = vim.fs.find({ ".git", "debug.bat" }, { upward = true })[1]
---
+++ nvim/lua/plugins/tokyonight.lua
@@ -0,0 +1,10 @@
+return {
+  "folke/tokyonight.nvim",
+  opts = {
+    on_highlights = function(highlights, colors)
+      highlights.LineNr.fg = colors.fg
+      highlights.LineNrAbove.fg = colors.fg
+      highlights.LineNrBelow.fg = colors.fg
+    end,
+  },
+}
